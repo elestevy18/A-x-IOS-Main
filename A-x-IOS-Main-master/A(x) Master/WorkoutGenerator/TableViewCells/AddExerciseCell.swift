3242e75730fa5a9fd46648ac2e4f144ac0e0cd3f
k0j07eh <kevin.jimenez@walmart.com>
2022-07-28 08:44:20 -0700

    sf

---
+++ A-x-IOS-Main-master/A(x) Master/WorkoutGenerator/TableViewCells/AddExerciseCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddExerciseCell.swift
+//  AofX
+//
+//  Created by Kevin Jimenez on 8/25/21.
+//  Copyright © 2021 Aesthet(X). All rights reserved.
+//
+
+import UIKit
+
+class AddExerciseCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
